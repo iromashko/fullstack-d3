67c97b10fcdb01414ee67013d1fd206b7f882e3b
iromashko <iromashko@me.com>
2021-03-19 12:51:07 +0300

    part 1

--- 01-making-your-first-chart/draft/chart.js
+++ 01-making-your-first-chart/draft/chart.js
@@ -1,6 +1,36 @@
-async function drawLineChart() {
-  // write your code here
+/// <reference types="d3" />
 
+async function drawLineChart() {
+  const data = await d3.json('../../my_weather_data.json');
+
+  const yAccessor = (d) => d['temperatureMax'];
+  const parseDate = d3.timeParse('%Y-%m-%d');
+  const xAccessor = (d) => parseDate(d['date']);
+
+  let dimensions = {
+    width: window.innerWidth * 0.9,
+    height: 400,
+    margins: {
+      top: 15,
+      right: 15,
+      bottom: 40,
+      left: 60,
+    },
+  };
+
+  dimensions.boundedWidth =
+    dimensions.width - dimensions.margins.left - dimensions.margins.right;
+
+  dimensions.boundedHeight =
+    dimensions.height - dimensions.margins.top - dimensions.margins.bottom;
+
+  const wrapper = d3
+    .select('#wrapper')
+    .append('svg')
+    .attr('width', dimensions.width)
+    .attr('height', dimensions.height);
+    
+  console.log(svg);
 }
 
-drawLineChart()
+drawLineChart();
